e81dbc822b3cc00342ff63e6e3a95d52cfc65fc1
Stefan <samardzicstefan6@gmail.com>
2024-03-21 14:35:51 +0100

    Inital UI setup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './shared/models/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'PC Store';
+  products: Product[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
 }
